27837bd422241df160b77923ad31c458c5341940
2swo <swlee456@naver.com>
2024-07-16 22:10:22 +0900

    (#3)route 분리- 1번쨰

---
+++ routes/home/route.js
@@ -0,0 +1,8 @@
+"user strict";
+
+const express = require("express");
+const router = express.Router(); // route
+
+router.post("/register", (req, res) => {}); //app -> route
+
+module.exports = router;
