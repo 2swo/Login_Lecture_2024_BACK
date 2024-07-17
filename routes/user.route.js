bd66139f91e5e54f501b539bde9318c57482cff8
2swo <swlee456@naver.com>
2024-07-18 01:17:40 +0900

    (#7)controller 로직설명, 역할설명

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,6 @@ const express = require("express");
 const userRouter = express.Router(); // route
 const userController = require("../controllers/user.controller.js");
 
-router.post("/register", userController.sign.register);
+router.post("/register", userController.register);
 
 module.exports = userRouter; // export 해줘야 app.js에서 라우터 받아오기
