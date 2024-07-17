227f13d765f9433b77007fc1721a3ba30678a461
2swo <swlee456@naver.com>
2024-07-17 17:44:01 +0900

    (#3)route 분리- 2번쨰

--- routes/home/route.js
+++
@@ -1,8 +0,0 @@
-"user strict";
-
-const express = require("express");
-const router = express.Router(); // route
-
-router.post("/register", (req, res) => {}); //app -> route
-
-module.exports = router; // export 해줘야 app.js에서 라우터 받아오기
