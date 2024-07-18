3a685be28836e7a12921f15c6590facaea1d8dad
2swo <swlee456@naver.com>
2024-07-18 15:27:51 +0900

    (#7)전체적 코드수정

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,9 @@ const express = require("express");
 const userRouter = express.Router(); // route
 const userController = require("../controllers/user.controller.js");
 
-router.post("/login/:id", userController.login);
+userRouter.post("/login", userController.login);
 
 module.exports = userRouter; // export 해줘야 app.js에서 라우터 받아오기
 
 //현재 고민중인것, index.js라는 route상위파일을 만들고, user.route.js파일은 하위파일로 만들어서, 각자의 route딴으로 넘기는것도 설명할가?
+//최적화할것 : route.js라는 route딴 상위파일에서, user.route.js 하위파일로 두게 한다. 모든 route는 route.js에서 받아오고, app.js에서는 route.js만 참조한다
--- services/user.service.js
+++ services/user.service.js
@@ -5,8 +5,8 @@ const mysql = require("mysql"); // mysql 가져옴.
 //db 연결 없이 더미데이터를가지고 테스트를 할지도 고민.
 //db 연결 없이 더미데이터를 가지고 테스트를 하는 것으로 실행.
 const users = {
-  id: [id1, id2, id3],
-  pw: [pw1, pw2, pw3],
+  id: ["id1", "id2", "id3"],
+  pw: ["pw1", "pw2", "pw3"],
 };
 
 // 모듈을 나누기 전, 먼저 회원가입 API-POST를 app.js 파일 하나에 다 넣고, 하나씩 분리하면서 설명
@@ -15,7 +15,23 @@ const users = {
 // 테스트의 어려움. 모든 로직이 한 파일에 있기 때문에, 버그가 발생했을 때 원인을 찾기 어렵습니다.
 
 const login = async (id, pw) => {
-  // id,패스워드 검증 로직 (레포지토리딴)
+  if (!id || !pw) {
+    //id와 패스워드를 입력했는지
+    return res
+      .status(400) //상태코드 간략한 설명
+      .json({ success: false, msg: "ID와 비밀번호를 입력하세요." });
+  }
+  const userIndex = users.id.indexOf(id);
+  // console.log(userIndex); //indexOf에 대한 간략한 설명 (배열에서 알맞은 값을 찾고 어떻게 반환하는지)
+  if (userIndex === -1) {
+    return res.status(400).json({ success: false, msg: "ID가 틀렸습니다." });
+  }
+  if (users.pw[userIndex] !== pw) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "비밀번호가 틀렸습니다." });
+  }
+  return { success: true, msg: "로그인 성공" };
 };
 
 module.exports = {
