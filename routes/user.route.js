fdd2ff8530270491a5c714343663d555bf4e063d
2swo <swlee456@naver.com>
2024-07-19 19:07:56 +0900

    (#7)ch2-2 user.route.js 최적화

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,33 +3,36 @@
 const express = require("express");
 const userRouter = express.Router(); // route
 
-userRouter.post("/login", login);
-
-const login = async (req, res) => {
+userRouter.post("/login", async (req, res) => {
   const { id, pw } = req.body;
+
+  // ID와 비밀번호 입력 검증
   if (!id || !pw) {
-    //id와 패스워드를 입력했는지 검증
     return res
-      .status(400) //여기서 statuscode에 대한 간략한 설명
+      .status(400) // 400: 잘못된 요청 (Bad Request)
       .json({ success: false, msg: "ID와 비밀번호를 입력하세요." });
   }
-  const response = await loginService(id, pw); // 변수명을 바꿀 필요 있을 것 같음.
 
-  const loginService = (id, pw) => {
-    const userIndex = users.id.indexOf(id);
-    // console.log(userIndex); //indexOf에 대한 간략한 설명 (배열에서 알맞은 값을 찾고 어떻게 반환하는지)
-    if (userIndex === -1) {
-      return res.json({ success: false, msg: "ID가 틀렸습니다." });
-    }
-    if (users.pw[userIndex] !== pw) {
-      return res.json({ success: false, msg: "비밀번호가 틀렸습니다." });
-    }
-    return { success: true, msg: "로그인 성공" };
-  };
+  const response = await loginService(id, pw); // 변수명 변경 (result -> response)
 
   if (response.success) {
-    return res.status(200).json(response);
+    return res.status(200).json(response); // 200: 성공 (OK)
+  } else {
+    return res.status(400).json(response); // 400: 잘못된 요청 (Bad Request)
   }
+});
+
+const loginService = (id, pw) => {
+  const userIndex = users.id.indexOf(id);
+
+  // indexOf: 배열에서 주어진 값의 첫 번째 인덱스를 반환. 값이 없으면 -1 반환
+  if (userIndex === -1) {
+    return { success: false, msg: "ID가 틀렸습니다." };
+  }
+  if (users.pw[userIndex] !== pw) {
+    return { success: false, msg: "비밀번호가 틀렸습니다." };
+  }
+  return { success: true, msg: "로그인 성공" };
 };
 
 module.exports = userRouter; // export 해줘야 app.js에서 라우터 받아오기
