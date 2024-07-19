4b51176cf4d61baa2fbbeb19bf992ddb14be0aef
2swo <swlee456@naver.com>
2024-07-20 02:12:03 +0900

    (#7)gitignore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express"); // 다운로드받은 express를 가져옴.
 const app = express(); // express 모듈의 function을 사용하여 새로운 express 앱을 만들겠다고 선언.
 const port = 3000; // port는 어플리케이션을 열 포트. 5000번 해도 되고 그럼. 이때 기본 port 설명을 할까말까.
+app.use(bodyParser.json()); // JSON 요청 본문 파싱 설정
 
 const users = {
   //db 대신 user 데이터 예시
